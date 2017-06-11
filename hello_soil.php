997eb6ad55a83802d5342b399d04ba7b07bf360a
yellowsoil <15078341635@163.com>
2017-06-11 10:53:29 +0800

    Add files via upload

---
+++ hello_soil.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Example</title>
+		<style type="text/css">
+			a {
+				background-color:grey;
+				color:white;
+			}
+			span {
+				border:thin black solid;
+				padding: 10px;
+			}
+		</style>
+	</head>
+	<body>
+		<a href="http£º//apress.com">Visit the Apress website</a>
+		<p>I like <span>apples</span> and oranges.</p>
+		<a href="http://w3c.org">Visit the W3C website</a>
+		<?php print "This is Our Soil!" ?>
+	</body>
+</html>
